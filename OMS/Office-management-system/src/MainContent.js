1f26fb1bbf516513e6d38703f47b237baa960f87
Nayan0406git config --global user.email <nayannikhare83@gmail.com>
2025-07-30 13:06:18 +0530

    hr attendance section remove

--- OMS/Office-management-system/src/MainContent.js
+++ OMS/Office-management-system/src/MainContent.js
@@ -41,7 +41,7 @@ import EmailDetails from "./Components/mail/EmailDetails";
 import Calender from "./Components/calender/calender";
 import Certificate from "./Components/Certificates/Certificate";
 import ViewDetails from "./Components/ViewDetails";
-import HrAttendance from "./Components/HrAttendance";
+// import HrAttendance from "./Components/HrAttendance";
 import HRRegistration from "./Components/HRRegistration/HRRegistration";
 import Invoice from "./Components/invoice/Invoice";
 import AdminDashboard from "./AdminDashboard.js";
@@ -121,11 +121,11 @@ const MainContent = ({ nav }) => {
     const roleSpecificItems = {
       Super_Admin: [
         { path: "/Db", label: "Employees", icon: <FiUsers /> },
-        {
-          path: "/hrAttendance",
-          label: "HR Attendance",
-          icon: <FiClipboard />,
-        },
+        // {
+        //   path: "/hrAttendance",
+        //   label: "HR Attendance",
+        //   icon: <FiClipboard />,
+        // },
         // { path: "/QuotationList", label: "Quotations", icon: <FiFileText /> },
         { path: "/Invoice", label: "Invoice", icon: <FiFileMinus /> },
         { path: "/Attendance", label: "Attendance", icon: <FiClipboard /> },
@@ -139,11 +139,11 @@ const MainContent = ({ nav }) => {
         { path: "/Db", label: "Employees", icon: <FiUsers /> },
         { path: "/certificate", label: "Certificate", icon: <FiFileText /> },
         { path: "/Attendance", label: "Attendance", icon: <FiClipboard /> },
-        {
-          path: "/hrAttendance",
-          label: "HR Attendance",
-          icon: <FiClipboard />,
-        },
+        // {
+        //   path: "/hrAttendance",
+        //   label: "HR Attendance",
+        //   icon: <FiClipboard />,
+        // },
         {
           path: "/hr-leave-application",
           label: "Apply Leave",
@@ -830,7 +830,7 @@ const MainContent = ({ nav }) => {
           <Route path="/Inbox/email-details" element={<EmailDetails />} />
           <Route path="/Calender" element={<Calender />} />
           <Route path="/Certificate" element={<Certificate />} />
-          <Route path="/hrAttendance" element={<HrAttendance />} />
+          {/* <Route path="/hrAttendance" element={<HrAttendance />} /> */}
           <Route path="/Invoice" element={<Invoice />} />
           <Route
             path="/super-admin-leave-management"
