dc995427e4f65a1e440501e7b4f3e2daf55a13f2
aaadi02 <adityadakhane011@gmail.com>
2025-08-08 13:29:01 +0530

    wsgi.py added

---
+++ OMS/face-recognition-server/start_dev_server.bat
@@ -0,0 +1,31 @@
+@echo off
+:: Face Recognition Server Development Startup Script for Windows
+
+:: Get the directory where this script is located
+cd /d "%~dp0"
+
+:: Check if virtual environment exists
+if not exist "venv" (
+    echo Creating virtual environment...
+    python -m venv venv
+)
+
+:: Activate virtual environment
+call venv\Scripts\activate.bat
+
+:: Install/update requirements
+echo Installing requirements...
+pip install -r requirements.txt
+
+:: Create necessary directories
+if not exist "images" mkdir images
+
+:: Start the server with Flask development server
+echo Starting Face Recognition Server in development mode...
+echo Server will be available at: http://0.0.0.0:5002
+echo Press Ctrl+C to stop the server
+
+:: Run directly with Python
+python server.py
+
+pause
