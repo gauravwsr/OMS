ca1efa3729eb07254ddfcdfe1ea6e54c15ac79bc
gaurav <gauravwasekar7@gmail.com>
2025-08-13 18:24:47 +0530

    cloudinary added

--- OMS/server-OMS/test_import.js
+++
@@ -1,5 +0,0 @@
-console.log('Testing import...');
-const { upload, uploadToCloudinary } = require('./config/cloudinaryConfig');
-console.log('Upload type:', typeof upload);
-console.log('uploadToCloudinary type:', typeof uploadToCloudinary);
-console.log('Upload.array method:', typeof upload.array);
